3cca868eea47adf2f00c78d86e957559d487c972
jorgermduarte <joka867@hotmail.com>
2022-10-29 02:19:38 +0100

    feat: added support to visualize the upgraded model results on the data-visualization

--- data-visualization/index.js
+++ data-visualization/index.js
@@ -38,12 +38,59 @@ server.get('/experiments/basic/:experiment', async (req, res) => {
         .SetData({
             experiment: JSON.stringify(data),
             experiment_name: experimentName,
-            default_experiment: JSON.stringify(default_experiment),
+            default_experiment: default_experiment != null ? JSON.stringify(default_experiment): null,
             model_type: "basic"
         })
         .Render('preview')
 });
 
+server.get('/experiments/upgraded/:experiment', async (req, res) => {
+
+    const requiredKeys = [
+        'BehaviorSpace results (NetLogo 6.3.0)',
+        '_1',
+        '_2',
+        '_3',
+        '_4',
+        '_5',
+        '_6',
+        '_7',
+        '_8',
+        '_9',
+        '_10',
+        '_11'
+    ];
+    const newKeys = [
+        "[run number]",
+        "yellow-food-percentage",
+        "n-shelter",
+        "expert-kill-basic-min-energy",
+        "n-basic-agent",
+        "n-expert-agent",
+        "green-food-percentage",
+        "basic-agent-camouflage-percentage",
+        "trap-percentage",
+        "[step]",
+        "count basic-agent",
+        "count expert-agent"
+    ]
+    const experimentName = req.params.experiment;
+    let default_experiment = null;
+
+    if(experimentName != 'upgraded-model-default-table.csv'){
+        default_experiment = await experimentLib.getExperiment("upgraded-model-default-table.csv",requiredKeys,newKeys);
+    }
+
+    const data = await experimentLib.getExperiment(experimentName,requiredKeys,newKeys);
+        new render(req,res)
+        .SetData({
+            experiment: JSON.stringify(data),
+            experiment_name: experimentName,
+            default_experiment: default_experiment != null ? JSON.stringify(default_experiment): null,
+            model_type: "upgraded"
+        })
+        .Render('preview')
+});
 server.listen(3000, () => console.log("server started successfully at port 3000"));
 
 
